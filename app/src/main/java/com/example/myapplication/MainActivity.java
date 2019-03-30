cc0fb2f17686b9892e9accd702fb3ad881c4a2ec
singhsters <littlesaurabh@gmail.com>
2019-03-30 20:06:00 +0530

    added comment

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-Button b;
+Button b,b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
